333782a7d9e750707b17ee8248acadb03b7f0835
LAPTOP-TP6PLN10\Logan <geng.luo@student.kuleuven.be>
2022-04-27 13:42:08 +0200

    beginning version

---
+++ app/src/main/java/be/kuleuven/travelrecipe/MainActivity.java
@@ -0,0 +1,49 @@
+package be.kuleuven.travelrecipe;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+    private ImageView imgMore;
+    private ImageView imgSetting;
+
+    private RecipeFragment recipeFragment;
+    private WorkFragment workFragment;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        ImageView imgMore = findViewById(R.id.img_More);
+        ImageView imgSetting = findViewById(R.id.img_Setting);
+
+        recipeFragment = new RecipeFragment();
+        workFragment = new WorkFragment();
+
+        tabLayout.setupWithViewPager(viewPager);
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        viewPagerAdapter.addFragment(recipeFragment,"recipe");
+        viewPagerAdapter.addFragment(workFragment,"work");
+        viewPager.setAdapter(viewPagerAdapter);
+
+    }
+
+    public void onImgSetting_Clicked(View caller){
+        startActivity(new Intent(this, Settings.class));
+    }
+}
