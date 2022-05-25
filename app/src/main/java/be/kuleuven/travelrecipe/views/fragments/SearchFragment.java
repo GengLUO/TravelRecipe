c6148ea27937c8a5ae7bffebf546ed05eebe4ce6
LAPTOP-TP6PLN10\Logan <geng.luo@student.kuleuven.be>
2022-05-25 12:10:37 +0200

    实现搜索功能

--- app/src/main/java/be/kuleuven/travelrecipe/views/fragments/SearchFragment.java
+++ app/src/main/java/be/kuleuven/travelrecipe/views/fragments/SearchFragment.java
@@ -3,8 +3,11 @@ package be.kuleuven.travelrecipe.views.fragments;
 import android.app.ProgressDialog;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -26,7 +29,10 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import be.kuleuven.travelrecipe.R;
 import be.kuleuven.travelrecipe.adapters.DashboardAdapter;
@@ -39,13 +45,15 @@ public class SearchFragment extends Fragment {
     RecyclerView dashboardRecyclerView;
     DashboardAdapter dashboardAdapter;
     List<Recipe> dashboardModelList;
+    SearchView searchView;
 
     private RequestQueue requestQueue;
     private static final String GET_IMAGE_URL = "https://studev.groept.be/api/a21pt210/getRecipe";
     private int PICK_IMAGE_REQUEST = 111;
     private Bitmap bitmap;
     private ProgressDialog progressDialog;
-    protected RecipesModel recipesModel = new RecipesModel();
+    //protected RecipesModel recipesModel = new RecipesModel();
+    private List<Recipe> recipes = new ArrayList<>();
 
     public SearchFragment() {
         // Required empty public constructor
@@ -68,7 +76,9 @@ public class SearchFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_search, container, false);
 
         dashboardRecyclerView = view.findViewById(R.id.recycler_view);
-
+        searchView = view.findViewById(R.id.searchView);
+        //searchView.clearFocus();
+        setSearchViewListeners();
         requestQueue = Volley.newRequestQueue(getContext());
 
         requestRecipes();
@@ -78,8 +88,38 @@ public class SearchFragment extends Fragment {
         return view;
     }
 
-    private void bindAdapter() {
-        dashboardAdapter = new DashboardAdapter(recipesModel.getAllRecipes(),getContext());
+    private void setSearchViewListeners() {
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                bindAdapter(filterList(newText));
+                return false;
+            }
+        });
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private List<Recipe> filterList(String text) {
+        List<Recipe> filteredList = new ArrayList<>();
+        filteredList = recipes.stream()
+                                .filter(r -> r.getName().contains(text))
+                                .collect(Collectors.toList());
+//        recipes.stream().forEach(e -> {
+//            if (e.getName().contains(text)){
+//                filteredList.add(e);
+//            }
+//        });
+        return filteredList;
+    }
+
+    private void bindAdapter(List<Recipe> recipes) {
+        dashboardAdapter = new DashboardAdapter(recipes,getContext());
         LinearLayoutManager layoutManager = new LinearLayoutManager(getContext(),LinearLayoutManager.VERTICAL,false);
         dashboardRecyclerView.setLayoutManager(layoutManager);
         dashboardRecyclerView.setNestedScrollingEnabled(false);
@@ -110,16 +150,13 @@ public class SearchFragment extends Fragment {
 
                                     //Link the bitmap to the ImageView, so it's visible on screen
                                     //imageRetrieved.setImageBitmap( bitmap2 );
-                                    recipesModel.addRecipe(new Recipe(name,desc,id,bitmap));
+                                    recipes.add(new Recipe(name,desc,id,bitmap));
 
                                     //Just a double-check to tell us the request has completed
-
-
-
                                 }
                                 progressDialog.dismiss();
                                 Toast.makeText(getContext(), "IIImage retrieved from DB", Toast.LENGTH_SHORT).show();
-                                bindAdapter();
+                                bindAdapter(recipes);
                             }
                         }
                         catch( JSONException e )
