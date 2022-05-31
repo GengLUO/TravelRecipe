1283a2391ece9aa7a94a9cb579eb0257a1289535
LAPTOP-TP6PLN10\Logan <geng.luo@student.kuleuven.be>
2022-05-31 18:25:31 +0200

    encrypt

--- app/src/main/java/be/kuleuven/travelrecipe/views/activities/LoginActivity.java
+++ app/src/main/java/be/kuleuven/travelrecipe/views/activities/LoginActivity.java
@@ -18,6 +18,8 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -85,7 +87,21 @@ public class LoginActivity extends AppCompatActivity {
         }
     }
 
-
-
+    public String encrypt(String password){
+        String encryptedPassword = null;
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-1");
+            byte[] bytes = md.digest(password.getBytes());
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < bytes.length; i++) {
+                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16)
+                        .substring(1));
+            }
+            encryptedPassword = sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return encryptedPassword;
+    }
 
 }
